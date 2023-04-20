9eaecdb9bea85050ee65fc2bf1b2852431963510
濱田広毅 <sac77qbc@gmail.com>
2023-04-20 14:10:11 +0900

    予約作成、詳細画面実装

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -5,7 +5,10 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreEventRequest;
 use App\Http\Requests\UpdateEventRequest;
 use App\Models\Event;
+use App\Service\EventService;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
+
 
 class EventController extends Controller
 {
@@ -31,7 +34,19 @@ class EventController extends Controller
      */
     public function store(StoreEventRequest $request)
     {
-        //
+        $check = EventService::CheckEventDuplication($request['event_date'], $request['start_time'], $request['end_time']);
+
+        if($check){
+            session()->flash('status', 'この時間帯にはすでに予約があります。');
+            return view('manager.events.create');
+        }
+
+        $startDate = EventService::JoinDateAndTime($request['event_date'], $request['start_time']);
+        $endDate = EventService::JoinDateAndTime($request['event_date'], $request['end_time']);
+        EventService::Store($request['event_name'], $request['information'], $request['max_people'],
+        $startDate, $endDate, $request['is_visible']);
+
+        return to_route('events.index');
     }
 
     /**
@@ -39,7 +54,11 @@ class EventController extends Controller
      */
     public function show(Event $event)
     {
-        //
+        $event = Event::findOrFail($event->id);
+        $event_date = $event->EventDate;
+        $start_date = $event->StartTime;
+        $end_date = $event->EndTime;
+        return view('manager.events.show', compact('event'));
     }
 
     /**
---
+++ app/Service/EventService.php
@@ -0,0 +1,43 @@
+<?php
+namespace App\Service;
+
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+use PhpParser\Node\Expr\FuncCall;
+use App\Models\Event;
+
+class EventService
+{
+
+    public static function CheckEventDuplication($event_date, $start_time, $end_time)
+    {
+        $check = DB::table('events')
+        ->whereDate('start_date', $event_date)
+        ->whereTime('end_date', '>', $start_time)
+        ->whereTime('start_date', '<', $end_time)
+        ->exists();
+
+        return $check;
+    }
+
+    public static function JoinDateAndTime($date, $time)
+    {
+        $join = $date. " ". $time;
+        $dateTIme = Carbon::createFromFormat('Y-m-d H:i', $join);
+        return $dateTIme;
+    }
+
+    public static function Store($event_name, $information, $max_people, $start_date, $end_date, $is_visible)
+    {
+        Event::create([
+            'name' => $event_name,
+            'information' => $information,
+            'max_people' => $max_people,
+            'start_date' => $start_date,
+            'end_date' => $end_date,
+            'is_visible' => $is_visible
+
+        ]);
+    }
+    
+}
